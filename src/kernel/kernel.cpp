782e0bb3128ba18a207298af001dd3f9bc59b27b
Ixxo Ixxo <drixxo@gmail.com>
2017-01-22 17:37:30 +0000

    initial layout

---
+++ src/kernel/kernel.cpp
@@ -0,0 +1,11 @@
+#include "kernel.hpp"
+
+namespace ixxor {
+
+int kernel_foo() 
+{
+    return 123;
+}
+
+} // close ixxor
+
---
+++ src/kernel/kernel.hpp
@@ -0,0 +1,11 @@
+#ifndef INCLUDED_KERNEL
+#define INCLUDED_KERNEL
+
+namespace ixxor {
+
+int kernel_foo();
+
+} // close ixxor
+
+#endif // INCLUDED_KERNEL
+
