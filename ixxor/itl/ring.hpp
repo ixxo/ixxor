ea386e2b2d6b8ddaf27f7f59165b9f59fc0f6a85
Ixxo Ixxo <drixxo@gmail.com>
2017-01-29 23:25:13 +0000

    Ring

--- ixxor/itl/ring.hpp
+++ ixxor/itl/ring.hpp
@@ -4,25 +4,33 @@
 #include <memory>
 #include <cstddef>
 #include <cassert>
+#include <iterator>
 
 namespace ixxor {
 namespace itl {
 
+template<typename T, bool is_const> 
+class ring_iterator;
+
+
 template<typename T>
 class ring
 {
+    template<class Tx, bool isConst> friend class ring_iterator;
+
+public:
     using size_type = std::size_t;
     using value_type = T;
+private:
 
     size_type pos_;
     size_type size_;
     size_type max_size_;
     std::unique_ptr<value_type[]> c_;
-    
 
 public:
-    class iterator {};
-    class const_iterator {};
+    using iterator = ring_iterator<T, false>;
+    using const_iterator = ring_iterator<T, true>;
 
 public:
     ring();
@@ -30,7 +38,8 @@ public:
     ring(ring&&);
     ring& operator=(ring const&);
     ring& operator=(ring&&);
-
+    ring(std::initializer_list<value_type> lst);
+  
     explicit ring(size_type count);
 
     bool empty() const;
@@ -45,8 +54,8 @@ public:
     template<class... Args>
     void emplace_back(Args&&...);
 
-    iterator begin() { return iterator{}; }
-    const_iterator begin() const { return const_iterator{}; }
+    iterator begin() { return iterator{this, 0}; }
+    const_iterator begin() const { return const_iterator{this, 0}; }
     iterator end() { return iterator{}; }
     const_iterator end() const { return const_iterator{}; }
 
@@ -66,17 +75,28 @@ ring<T>::ring():
 }
 
 template<class T>
+ring<T>::ring(std::initializer_list<value_type> lst):
+    pos_(0u),
+    size_(lst.size()),
+    max_size_(lst.size()),
+    c_(new value_type[lst.size()])
+{
+    if (size_) {
+        std::copy(std::begin(lst), std::end(lst), c_.get());
+    }
+}
+
+template<class T>
 ring<T>::ring(ring const& other)
 {
     if (other.max_size()) {
         c_.reset(new value_type[other.max_size()]);
     }
-    assert(false);
+    max_size_ = other.max_size_;
     size_ = other.size_;
     pos_ = 0;
-    max_size_ = other.max_size_;
     if (size_) {
-        //std::copy(other.begin(), other.end(), other.c_.get());
+        std::copy(other.begin(), other.end(), begin());
     }
 }
 
@@ -108,8 +128,7 @@ ring<T>& ring<T>::operator=(ring<T> const& other)
     max_size_ = other.max_size_;
     if (max_size_) {
         c_.reset(new value_type[max_size_]);
-        assert(false);
-        //std::copy(other.begin(), other.end(), c_.get());
+        std::copy(other.begin(), other.end(), begin());
     }
     return *this;
 }
@@ -128,7 +147,7 @@ ring<T>::ring(size_type count):
 template<class T>
 bool ring<T>::empty() const
 {
-    return size_;
+    return size_==0;
 }
 
 template<class T>
@@ -178,6 +197,146 @@ void ring<T>::push_back(value_type const& value)
 {
     return emplace_back(value);
 }
+
+template<typename T, bool is_const> 
+class ring_iterator
+{
+public:
+
+    using Tx = typename std::conditional<is_const, T const, T>::type;
+    using ringx = typename std::conditional<is_const, ring<T> const, ring<T> >::type;
+    using value_type = T;
+    using reference = Tx&;
+    using pointer = Tx*;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::random_access_iterator_tag;
+
+    ring_iterator()
+        : obj_(nullptr), idx_(0)
+    {
+    }
+
+    ring_iterator(ring_iterator const& other) = default;
+    ring_iterator(ring_iterator&& other) = default;
+    
+    ring_iterator& operator=(ring_iterator const& other) = default;
+    ring_iterator& operator=(ring_iterator&& other) = default;
+
+    ring_iterator& operator++()
+    {
+        if (!obj_) {
+            return *this;
+        }
+        ++idx_;
+        if (idx_ >= obj_->size_) {
+            obj_ = nullptr;
+            idx_ = 0;
+        }
+        return *this;
+    }
+    
+    ring_iterator& operator+=(difference_type dif)
+    {
+        if (!obj_) {
+            return *this;
+        }
+        idx_ += dif;
+        if (idx_ >= obj_->size_) {
+            obj_ = nullptr;
+            idx_ = 0;
+        }
+        return *this;
+    }
+
+    ring_iterator& operator--()
+    {
+        if (!obj_) {
+            return *this;
+        }
+        if (idx_) {
+            --idx_;
+        } else {
+            obj_ = nullptr;
+            idx_ = 0;
+        }
+        return *this;
+    }
+    
+    ring_iterator& operator-=(difference_type dif)
+    {
+        if (!obj_ || dif > idx_) {
+            obj_ = nullptr;
+            idx_ = 0;
+            return *this;
+        }
+        idx_ -= dif;
+        return *this;
+    }
+
+    ring_iterator operator+(difference_type dif) const
+    {
+        ring_iterator copy{*this};
+        copy += dif;
+        return copy;
+    }
+
+    ring_iterator operator-(difference_type dif) const
+    {
+        ring_iterator copy{*this};
+        copy -= dif;
+        return copy;
+    }
+
+    ring_iterator operator++(int)
+    {
+        auto prev = *this;
+        ++(*this);
+        return prev;
+    }
+    
+    ring_iterator operator--(int)
+    {
+        auto prev = *this;
+        --(*this);
+        return prev;
+    }
+
+    reference operator[](difference_type n) const
+    {
+        return obj_->c_.get()[(obj_->pos_ + idx_ + n) % obj_->max_size_];
+    }
+
+    bool operator==(ring_iterator const& other) const
+    {
+        return obj_ == other.obj_ && (!obj_ || idx_ == other.idx_);
+    }
+    bool operator!=(ring_iterator const& other) const
+    {
+        return !operator==(other);
+    }
+
+    reference operator*() const
+    {
+        return obj_->c_.get()[(obj_->pos_ + idx_) % obj_->max_size_];
+    }
+
+
+private:
+    explicit ring_iterator(ringx* obj, std::size_t idx):
+        obj_(obj), idx_(idx)
+    {
+        if (idx_ >= obj_->size_) {
+            obj_ = nullptr;
+            idx_ = 0;
+        }
+    }
+
+    template<class Tx>
+    friend class ring;
+
+    ringx* obj_;
+    std::size_t idx_;
+};
 
 
 } // :: ixxor::itl
